b3ee9662726f76a8eb65894c515d498906f54319
stan.hordon <stan.hordon@gmail.com>
2023-11-27 18:48:44 -0800

    add server for upload images

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
 import requests
+import os
 
 # This example shows how a file can be uploaded using
 # The Python Requests module
 
+# url = "http://localhost/upload/"
+# with open('/usr/share/apache2/icons/icon.sheet.png', 'rb') as opened:
+#     r = requests.post(url, files={'file': opened})
+
 url = "http://localhost/upload/"
-with open('/usr/share/apache2/icons/icon.sheet.png', 'rb') as opened:
-    r = requests.post(url, files={'file': opened})
+
+for file in os.listdir("~/supplier-data/images"): #Iterating through Files in the folder/directory
+    if file.endswith(".jpeg"): # Iterate through JPEG files only
+        with open(os.path.join("~/supplier-data/images", file), "rb") as opened: #opening jpeg files
+            r = requests.post(url, files={"file": opened}) # sending jpeg files on server
+            if r.status_code == 200: 
+                print(success)
+            else:
+                print(f" {file} error")
